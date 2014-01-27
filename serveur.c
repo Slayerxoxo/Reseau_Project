efeb733c9d3b9bbbe954e4b8af71e4a908da0838
Vincent Raveneau <vincentraveneau@orange.fr>
2014-01-28 00:47:51 +0100

    Passage des paramètres (nbr de salles et de joueurs) au serveur

--- serveur.c
+++ serveur.c
@@ -6,26 +6,40 @@ Serveur à lancer avant le client
 #include <SFML/Network.h>
 
 int main(int argc, char **argv) {
+	unsigned int maxRooms;
+	unsigned int playersPerRoom;
+
 	sfSocketUDP* socket = sfSocketUDP_Create();
 	char receptionBuffer[128];
 	size_t* received = NULL;
 	sfIPAddress* sender = NULL;
 	unsigned short* port = NULL;
 	
+	// Traitement des paramètres passés par l'utilisateur
+	if (argc != 3) {
+		perror("usage : serveur <nombre-max-de-salles (>0)> <nombre-de-joueurs-par-salle (1-4)>");
+		exit(1);
+    }
+    maxRooms = atoi(argv[1]);
+    playersPerRoom = atoi(argv[2]);
+    printf("Ce serveur gère au maximum %i parties simultanées, avec %i joueurs par partie.\n",maxRooms, playersPerRoom);
+	
+	// Liaison de la socket d'écoute des demandes de connexion au port 5000
 	if(!sfSocketUDP_Bind(socket,5000)) {
 		perror("erreur : impossible d'affecter le port 5000 à la socket du serveur.\n");
 		exit(1);
 	} else {
-		printf("Socket d'écoute des connexions du serveur liée au port 5000\n");
+		printf("Socket d'écoute des demandes de connexion du serveur liée au port 5000.\n");
 	}
 
-	
+	// Ecoute des demandes de connexion
 	if (sfSocketUDP_Receive(socket, receptionBuffer, sizeof(receptionBuffer), received, sender, port) != sfSocketDone)
 	{
 		perror("erreur : impossible d'établir la connexion avec le client.\n");
 		exit(1);
 	}
 
+	// Fermeture de la socket d'écoute des demandes de connexion
 	sfSocketUDP_Destroy(socket);
 	
 	return(EXIT_SUCCESS);
